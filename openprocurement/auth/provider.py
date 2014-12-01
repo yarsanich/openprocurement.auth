9a15a2519c0c6f9b64a7e05090d10a0a586b6c75
Taras Kozlovskyi <ktarasz@quintagroup.com>
2014-12-01 16:42:47 +0200

    Create db on start

--- openprocurement/auth/provider.py
+++ openprocurement/auth/provider.py
@@ -10,11 +10,11 @@ def make_oath_provider_app(
         sqlite='sqlite:///db.sqlite',
         secret='abcdfg',
         timezone='Europe/Kiev'):
-
     oauth_provider.config.update({
         'SQLALCHEMY_DATABASE_URI': sqlite,
     })
     oauth_provider.secret_key = secret
+    db.create_all()
     return oauth_provider
 
 if __name__ == '__main__':
