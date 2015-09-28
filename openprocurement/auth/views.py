1351c7294833f1ecbe5162ba842a96f2fb319d3c
Taras Kozlovskyi <ktarasz@quintagroup.com>
2015-09-28 17:02:33 +0300

    Response fix

--- openprocurement/auth/views.py
+++ openprocurement/auth/views.py
@@ -1,6 +1,6 @@
 from openprocurement.auth.provider_app import oauth_provider, oauth
 from openprocurement.auth.models import current_user
-from flask import request, redirect, jsonify, render_template
+from flask import request, redirect, jsonify, render_template, make_response
 
 
 @oauth_provider.route('/oauth/token', methods=['GET', 'POST'])
@@ -20,7 +20,7 @@ def authorize(*args, **kwargs):
         kwargs['client'] = oauth_provider.auction_client
         kwargs['user'] = user
         kwargs['redirect_uri'] = request.args['redirect_uri']
-        response = render_template('authorize.html', **kwargs)
+        response = make_response(render_template('authorize.html', **kwargs))
         response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
         response.headers['Pragma'] = 'no-cache'
         response.headers['Expires'] = '-1'
