b30207d31c97aca65c9c6214c766ae198f9f791a
Leits <leits@quintagroup.org>
2016-01-11 18:41:44 +0200

    Logging for authorize and bid_allow

---
+++ openprocurement/auth/systemd_msgs_ids.py
@@ -0,0 +1,7 @@
+import uuid
+
+#log ID for authorize_handler
+AUTH_GET_FORM = uuid.UUID('16fb22d88dbf4997809d20c646c7f9f3')
+AUTH_CONFIRM_FORM = uuid.UUID('8ece6d5cc77a4ebd94a4b6b2417b34b3')
+AUTH_NOT_CONFIRM_FORM = uuid.UUID('eedb5ed943624a058d4c1dbfbc364872')
+AUTH_BID_ALLOW = uuid.UUID('a5bcc0c4264e4fa28878026ed76fe09f')
